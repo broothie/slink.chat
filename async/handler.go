e613f0ffb8e765a649419dad9a2ac5991c9d3804
broothie <adbooth8@gmail.com>
2025-03-11 21:16:41 -0700

    use raw body when cloud scheduler

--- async/handler.go
+++ async/handler.go
@@ -6,25 +6,34 @@ import (
 	"net/http"
 )
 
+const googleCloudScheduleUserAgent = "Google-Cloud-Scheduler"
+
 type Dispatch func(context.Context, Message) error
 
 func (a *Async) Handler(dispatch Dispatch) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var pubSubMessage struct {
-			Message struct {
-				Data []byte `json:"data,omitempty"`
-			} `json:"message"`
-		}
-
-		if err := json.NewDecoder(r.Body).Decode(&pubSubMessage); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
 		var message Message
-		if err := json.Unmarshal(pubSubMessage.Message.Data, &message); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+		if r.UserAgent() == googleCloudScheduleUserAgent {
+			if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		} else {
+			var pubSubMessage struct {
+				Message struct {
+					Data []byte `json:"data,omitempty"`
+				} `json:"message"`
+			}
+
+			if err := json.NewDecoder(r.Body).Decode(&pubSubMessage); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			if err := json.Unmarshal(pubSubMessage.Message.Data, &message); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
 		}
 
 		if err := dispatch(r.Context(), message); err != nil {
